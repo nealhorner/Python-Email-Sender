121c30bef6648dbb974f1b05c7d0277b21185602
nhhorner <thenealhorner@gmail.com>
2015-04-30 11:35:19 -0700

    initial commit

---
+++ script.py
@@ -0,0 +1,26 @@
+import smtplib
+FROM = 'Me' # Sender's Name
+TO = [] # List of addresses to send to. Ex. TO = ["example@example.com"]
+SUBJECT = "Hello!" # Standard email subject
+TEXT = "This message was sent with Python's smtplib."
+message = """\
+From: %s
+To: %s
+Subject: %s
+
+%s
+""" % (FROM, ", ".join(TO), SUBJECT, TEXT)
+username = "" # gmail username
+password = "" # gmail password
+message = """\
+From: %s
+To: %s
+Subject: %s
+%s
+""" % (FROM, ", ".join(TO), SUBJECT, TEXT)
+server = smtplib.SMTP('smtp.gmail.com', 587) # SMTP connection to gmail
+server.ehlo()
+server.starttls()
+server.login(username, password)
+server.sendmail(FROM, TO, message)
+server.quit()
