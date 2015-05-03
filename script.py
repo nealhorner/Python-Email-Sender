f655eb203501173308a721745fcc77fa30585218
nealhorner <thenealhorner@gmail.com>
2015-05-02 17:28:00 -0700

    Made script run as a function

--- script.py
+++ script.py
@@ -1,24 +1,27 @@
 import smtplib
 
+def sendEmail(FROM, TO, CC, SUBJECT, TEXT, UN, PW):
+    message = '''From: %s
+To: %s
+CC: %s
+Subject: %s
+    
+%s''' % (FROM, ", ".join(TO), CC, SUBJECT, TEXT)
+    print(message)
+    server = smtplib.SMTP('smtp.gmail.com', 587) # SMTP
+    server.ehlo()
+    server.starttls()
+    server.login(UN, PW)
+    server.sendmail(FROM, TO, message)
+    server.quit()
+
 FROM = 'Me' # Sender's Name
-TO = [] # List of addresses to send to. Ex. TO = ["example@example.com"]
+TO = [] # List of addresses to send to
+CC = ""
 SUBJECT = "Hello!" # Standard email subject
 TEXT = "This message was sent with Python's smtplib."
-
-message = """\
-From: %s
-To: %s
-Subject: %s
-
-%s
-""" % (FROM, ", ".join(TO), SUBJECT, TEXT)
 
 username = "" # gmail username
 password = "" # gmail password
 
-server = smtplib.SMTP('smtp.gmail.com', 587) # SMTP connection to gmail
-server.ehlo()
-server.starttls()
-server.login(username, password)
-server.sendmail(FROM, TO, message)
-server.quit()
+sendEmail(FROM, TO, CC, SUBJECT, TEXT, username, password)
