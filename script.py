4b084cf8f38400cffe0b2dea963572a3c8cdae0b
Neal Horner <thenealhorner@gmail.com>
2015-05-15 19:33:51 -0700

    simplified function

--- script.py
+++ script.py
@@ -1,27 +1,13 @@
 import smtplib
 
-def sendEmail(FROM, TO, CC, SUBJECT, TEXT, UN, PW):
-    message = '''From: %s
-To: %s
-CC: %s
-Subject: %s
-    
-%s''' % (FROM, ", ".join(TO), CC, SUBJECT, TEXT)
-    print(message)
-    server = smtplib.SMTP('smtp.gmail.com', 587) # SMTP
+def send(FROM,TO,SUBJECT,TEXT,SERVICE,PORT,USERNAME,PASSWORD):
+    message = """From: %s\nTo: %s\nSubject: %s\n\n%s""" % (FROM, ", ".join(TO),SUBJECT,TEXT)
+    server = smtplib.SMTP(SERVICE, PORT)
     server.ehlo()
     server.starttls()
-    server.login(UN, PW)
-    server.sendmail(FROM, TO, message)
+    server.login(USERNAME, PASSWORD)
+    server.sendmail(FROM,TO,message)
     server.quit()
 
-FROM = 'Me' # Sender's Name
-TO = [] # List of addresses to send to
-CC = ""
-SUBJECT = "Hello!" # Standard email subject
-TEXT = "This message was sent with Python's smtplib."
-
-username = "" # gmail username
-password = "" # gmail password
-
-sendEmail(FROM, TO, CC, SUBJECT, TEXT, username, password)
+# Example
+send("Your Name",["thenealhorner@gmail.com"],"Subject","Text","smtp.gmail.com",587,"server27314","encryption")
