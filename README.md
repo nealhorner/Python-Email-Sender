e26ed062394e45c9bf5fbec378c158bea4a1d424
sudoKIngpin <wd51886@gmail.com>
2021-10-02 23:20:46 +0530

    Updated script.py and readme file

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Python-Email-Sender
 A simple python function that sends emails using [smtp](http://en.wikipedia.org/wiki/Simple_Mail_Transfer_Protocol).
 
-[smtplib documentation](https://docs.python.org/2.7/library/smtplib.html)
+[smtplib documentation][Python3](https://docs.python.org/3/library/smtplib.html)
 
-Use the function to connect to a smtp server, using the correct URL and port number. For example Gmail's smtp server is smtp.gmail.com.
+Gmail's smtp server is smtp.gmail.com and port is 587. 
+
+
 [gmail documentation](https://support.google.com/a/answer/176600?hl=en)
--- script.py
+++ script.py
@@ -1,13 +1,39 @@
 import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import webbrowser 
 
-def send(FROM,TO,SUBJECT,TEXT,SERVICE,PORT,USERNAME,PASSWORD):
-    message = """From: %s\nTo: %s\nSubject: %s\n\n%s""" % (FROM, ", ".join(TO),SUBJECT,TEXT)
-    server = smtplib.SMTP(SERVICE, PORT)
-    server.ehlo()
-    server.starttls()
-    server.login(USERNAME, PASSWORD)
-    server.sendmail(FROM,TO,message)
-    server.quit()
 
-# Example
-send("Your Name",["name@domain.com"],"Subject","Text","smtp.gmail.com",587,"username","password")
+Reciever_add=input('Enter the mail address of reciever: ')
+
+BOT_ID='xyz@gmail.com'
+BOT_PWD='xyz_tech@#'
+server=smtplib.SMTP('smtp.gmail.com',587) #HOST AND PORT
+server.ehlo()
+server.starttls()#Transport layer secuirity encryption
+server.login(BOT_ID,BOT_PWD)
+
+Msg=MIMEMultipart()
+
+Subject=input('Enter Subject of mail:')
+body=input('Enter body of mail:')
+body=body+'\n'+ '\n' + "Thank you"+ '\n' +"Xyz Corporations"
+
+Msg["From"]=BOT_ID
+Msg['To']=Reciever_add
+Msg["Subject"]=Subject
+Msg.attach(MIMEText(body,'plain')) #ATTACHING BODY
+content=Msg.as_string()
+server.sendmail(BOT_ID,Reciever_add,content)
+print()
+o=f"MAIL SENT SUCCESSFULLY TO {Reciever_add}"
+
+server.quit()  #Terminating the session 
+
+webbrowser.open("https://github.com/nealhorner")
+#Redirecting user to developer's page 
+
+print("Please follow me on github if you are a true nerd !!!")
+[print()]
+print("Bye Dude !!!")
+exit() 
